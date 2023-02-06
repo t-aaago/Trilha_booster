b7e8bc51845074cb37d916860fe101687bd1dd7f
t-aaago <tiagoliveira003@gmail.com>
2023-02-06 00:23:11 -0300

    configuração do banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,8 @@
 import express from 'express';
+import routes from './routes'    
 
 const app = express();
-
-app.use(express.json())
-
-const data = [
-    'tiago',
-    'diego',
-    'castro',
-    'luan',
-    'nina',
-    'pedro'
-]
-
-app.get('/users', (request, response) =>{
-    const search = String(request.query.search)
-    const filtredUsers = search ? data.filter(user => user.includes(search)) : data
-    return response.json(filtredUsers)
-}); 
-
-app.get('/users/:id', (request, response) =>{
-    const id = Number(request.params.id);
-    const user = data[id]
-    return response.json(user)
-})
-
-app.post('/users', (request, response) =>{
-    const dados = request.body
-    const user = {
-        nome: dados.nome,
-        email: dados.email
-    }
-    return response.json(user)
-   
-})
-
-
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
